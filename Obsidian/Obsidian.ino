36a8a31b5e6d3b1c7a37dc120914bae323f9ee44
ishotjr <github@ishotjr.com>
2016-03-20 20:49:30 -0400

    :boom: :boom: Add multiple projectiles

--- Obsidian/Obsidian.ino
+++ Obsidian/Obsidian.ino
@@ -24,20 +24,27 @@
 #include <Gamebuino.h>
 Gamebuino gb;
 
+extern const byte font3x3[];
+
 // Obsidian spacecraft position and size
 int craft_x = LCDWIDTH / 8;
 int craft_y = LCDHEIGHT / 2;
 int craft_size = 4;
 
-// projectile position, size and state
-int projectile_x = 0;
-int projectile_y = 0;
-int projectile_size = 2;
+// projectile(s) position, size and state
+const int projectiles = 3;
+int projectileIndex = 0;
+int projectile_x[projectiles];
+int projectile_y[projectiles];
+int projectile_size[projectiles];
+int projectile_speed_x[projectiles];
 bool projectile_active = false;
+bool projectile_available = true;
 
 void setup() {
 
   gb.begin();
+  // remember to use F() for strings in order to save RAM!
   gb.titleScreen(F("Obsidian"));
 
 }
@@ -65,14 +72,22 @@ void loop() {
     // shoot
     if (gb.buttons.pressed(BTN_A)) {
 
-      // only shoot if projectile not already active
-      if (!projectile_active) {
+      // only shoot if max. projectiles not already active
+      if (projectile_available) {
 
         projectile_active = true;
         
         // start in front/center of craft
-        projectile_x = craft_x + craft_size;
-        projectile_y = craft_y + ((craft_size - projectile_size) / 2);
+        
+        // TODO: find available slot and fill
+
+        projectile_size[projectileIndex] = 2;
+        projectile_speed_x[projectileIndex] = 1;
+        projectile_x[projectileIndex] = craft_x + craft_size;
+        projectile_y[projectileIndex] = craft_y + ((craft_size - projectile_size[projectileIndex]) / 2);
+
+        // TODO: this is not really viable once projectiles start hitting things
+        projectileIndex = (projectileIndex + 1) % projectiles;
         
         gb.sound.playOK();
       }
@@ -87,19 +102,41 @@ void loop() {
     // update craft position
     gb.display.fillRect(craft_x, craft_y, craft_size, craft_size);
 
-    // update projectile position
+    // update projectile(s) position - skip if none active
     if (projectile_active) {
-      gb.display.fillRect(projectile_x, projectile_y, projectile_size, projectile_size);
-      // continue forward to edge of screen
-      // TODO: collision w/ enemy!
-      if (projectile_x < LCDWIDTH) {
-        projectile_x++;
-      } else {
-        // it's gone!
-        projectile_active = false;
-      }
+
+      // TODO: add projectile_active setting to skip this if none
+      
+      for (int i = 0; i < projectiles; i++) {
+
+        // ignore if immobile (?)
+        if (projectile_speed_x[i] > 0) {
+          gb.display.fillRect(projectile_x[i], projectile_y[i], projectile_size[i], projectile_size[i]);
+        }
+        
+        // continue forward to edge of screen
+        // TODO: collision w/ enemy!
+
+        if (projectile_x[i] < LCDWIDTH) {
+          projectile_x[i] += projectile_speed_x[i];
+        } else {
+          // it's gone - free up slot
+          projectile_x[i] = 0;
+          projectile_speed_x[i] = 0;
+          projectile_available = true;
+        }
+        
+      } 
+    
     }
 
+    // display CPU load during dev
+    gb.display.setFont(font3x3);
+    gb.display.cursorX = LCDWIDTH - (8 * 4);
+    gb.display.cursorY = LCDHEIGHT - 4;
+    gb.display.print(F("CPU:"));
+    gb.display.print(gb.getCpuLoad());
+    gb.display.print(F("%"));
   }
   
 }
